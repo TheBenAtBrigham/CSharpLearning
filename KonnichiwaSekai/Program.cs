60f4cb7cb35575547ec3023b8f5dea7874053f00
TheBenAtBrigham <160558577+TheBenAtBrigham@users.noreply.github.com>
2024-04-24 00:39:57 -0600

    Add files via upload

---
+++ KonnichiwaSekai/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string English = "Hello, World!";
+string Japanese = "こんにちは、世界！";
+Console.WriteLine(English);
+Console.WriteLine(Japanese);
+//cd KonnichiwaSekai
